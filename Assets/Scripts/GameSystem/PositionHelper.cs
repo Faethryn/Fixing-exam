d2095d4e2391f107aa50ace0865f2e2a6daed19c
Faethryn <laurens.muylle@student.howest.be>
2022-01-10 13:14:06 +0100

    fixed formulas

--- Assets/Scripts/GameSystem/PositionHelper.cs
+++ Assets/Scripts/GameSystem/PositionHelper.cs
@@ -13,17 +13,18 @@ namespace DAE.GameSystem
         public Vector2 ToGridPosition(Vector3 worldPosition)
         {
 
-            var q = ((2f / 3f) * worldPosition.x) / TileRadius;
-            var r = ((-1f / 3f) * worldPosition.x) + (Mathf.Sqrt(3f) / 3f * worldPosition.z) / TileRadius;
+            //var q = ((2f / 3f) * worldPosition.x) / TileRadius;
+            //var r = ((-1f / 3f) * worldPosition.x) + (Mathf.Sqrt(3f) / 3f * worldPosition.z) / TileRadius;
+            var q = ((Mathf.Sqrt(3f) / 3f) * worldPosition.x -1f/3f*worldPosition.z) / TileRadius;
+            var r = (2f / 3f * worldPosition.z) / TileRadius;
 
             var x = Mathf.RoundToInt(q);
             var y = Mathf.RoundToInt(r);
 
 
-            var worldX = ((3f / 2f) * x) * TileRadius;
-            var worldY = (float)(Mathf.Sqrt(3f) / 2f * x + Mathf.Sqrt(3f) * y) * TileRadius;
+            Vector3 wordPosition2 = ToWorldPosition(x, y);
 
-            Debug.DrawRay(new Vector3(worldX, 0, worldY), Vector3.down, Color.green, 10f);
+            Debug.DrawRay(new Vector3(wordPosition2.x, 0, wordPosition2.z), Vector3.down, Color.green, 10f);
 
             return new Vector2(x, y);
         }
@@ -31,8 +32,11 @@ namespace DAE.GameSystem
         public Vector3 ToWorldPosition(int x, int y)
         {
 
-            var q = TileRadius * ((3f / 2f) * x);
-            var r = TileRadius * (Mathf.Sqrt(3f) / 2f * x + Mathf.Sqrt(3f) * y);
+            //var q = TileRadius * ((3f / 2f) * x);
+            //var r = TileRadius * (Mathf.Sqrt(3f) / 2f * x + Mathf.Sqrt(3f) * y);
+
+            var q = (Mathf.Sqrt(3f) * x + Mathf.Sqrt(3f) / 2f * y) * TileRadius;
+            var r = (3f / 2f * y) * TileRadius;
 
             var tileposition = new Vector3(q, 0, r);
 
